3b113720028fdb89fd0ea746ac1e9afd05ab3e62
ashikur311 <ashikur31169@gmail.com>
2025-06-19 17:20:35 +0600

    this is the initial commit

---
+++ src/server.js
@@ -0,0 +1,67 @@
+//import
+import express from "express";
+import { ENV } from "./config/env.js";
+import {db} from "./config/db.js";
+import { favoritesTable } from "./db/schema.js";
+
+//consttant
+const app = express();
+const PORT = process.env.PORT || 8001;
+
+
+app.use(express.json()); // Parse JSON bodies if it not added this line all post field was undefined
+
+app.get("/api/health", (req, res) => { res.status(200).json({ success: true });});
+
+
+app.post("/api/favorites",async (req,res)=>{
+  try{
+    const {userId, recipeId, title, image, cookTime, servings} = req.body;
+    if(!userId || !recipeId || !title) {
+      return res.status(400).json({ error: "Missing required fields" });
+    }
+    const newFavorite= await db.insert(favoritesTable).values({userId,recipeId,title, image,cookTime, servings}).returning();
+    res.status(201).json(newFavorite[0]);
+  }catch (error){
+    console.error("Error adding favorite:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+}
+)
+
+
+app.get("/api/favorites/:userId",async (req,res)=>{
+  try{
+    const { userId}=req.params;
+    if(!userId) {
+      return res.status(400).json({ error: "User ID is required" });
+    }
+    const userfavorites= await db.select().from(favoritesTable).where(eq(favoritesTable.userId,userId))
+    res.status(200).json( userfavorites);
+
+  }catch(error){
+    console.error("Error fetching favorites:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+})
+
+
+app.delete("/api/favorites/:userId/:recipeId", async (req,res)=>{
+  try{
+const {userId, recipeId}=req.prarams
+await db.delete(favoritesTable).whwere(and(eq(favoritesTable.userId,userId),eq(favoritesTable.recipeId,parseInt(recipeId))))
+res.status(200).json({ message: "Favorite deleted successfully" });
+  }catch(error){
+    console.error("Error deleting favorite:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+})
+
+
+
+
+
+
+app.listen(PORT, () => {
+  console.log("Server is running on port:", PORT);
+});
